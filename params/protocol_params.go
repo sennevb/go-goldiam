0f381111afb247b8b227616c185dfb705d4dc5ea
Nenad Balog <n3nadba1og@gmail.com>
2018-01-04 18:58:03 +0100

    genesis giff and minimum diff changed

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -77,7 +77,7 @@ var (
 	GenesisGasLimit        = big.NewInt(4712388)               // Gas limit of the Genesis block.
 	TargetGasLimit         = new(big.Int).Set(GenesisGasLimit) // The artificial target
 	DifficultyBoundDivisor = big.NewInt(2048)                  // The bound divisor of the difficulty, used in the update calculations.
-	GenesisDifficulty      = big.NewInt(131072)                // Difficulty of the Genesis block.
-	MinimumDifficulty      = big.NewInt(131072)                // The minimum that the difficulty may ever be.
-	DurationLimit          = big.NewInt(8)                    // Time of the block The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
+	GenesisDifficulty      = big.NewInt(231072)                // Difficulty of the Genesis block.
+	MinimumDifficulty      = big.NewInt(231072)                // The minimum that the difficulty may ever be.
+	DurationLimit          = big.NewInt(8)                     // Time of the block The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
 )
